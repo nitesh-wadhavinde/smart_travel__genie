ac189b4d3998c57c2cb9b97ce1e8dd9bae9c0e40
nitesh-wadhavinde <niteshwadhavinde.nw@gmail.com>
2025-07-13 13:23:01 +0530

    First commit

---
+++ tools/arithmatic_op_tool.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.tools import tool
+from langchain_community.utilities.alpha_vantage import AlphaVantageAPIWrapper
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """
+    Multiply two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The product of a and b.
+    """
+    return a * b
+
+@tool
+def add(a: int, b: int) -> int:
+    """
+    Add two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    return a + b
